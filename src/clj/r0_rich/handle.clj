70caba34effba754a199de8af4e9808334f23a46
gzmask <gzmask@gmail.com>
2013-06-13 16:52:38 -0600

    proper css generation as function

--- src/clj/r0_rich/handle.clj
+++ src/clj/r0_rich/handle.clj
@@ -7,16 +7,14 @@
             [compojure.handler :as handler]
             [compojure.route :as route]))
 
-(def css
-  (gaka/css [:div.navigation_bar 
-             :background-color "#ccc"
-             :color "#fff" 
-             :height "25px"
-             :padding "5px 5px 0px 50px"]
-            [:body
-             :background-color "#444"]
-            [:i.icon-camera-retro
-             :color "#000"]))
+(defn css [] 
+  {:headers {"Content-Type" "text/css"}
+   :body (gaka/css [:body :background-color "#444"
+                   [:div.navigation_bar :background-color "#ccc" 
+                                        :color "#fff" 
+                                        :height "25px" 
+                                        :padding "5px 5px 0px 50px"]
+                   [:i.icon-camera-retro :color "#000"]])})
 
 (defn index-page []
   "index page for richever"
@@ -26,7 +24,7 @@
     (include-css "/vendor/bootstrap/css/bootstrap.min.css")
     (include-css "/vendor/bootstrap/css/bootstrap-responsive.css")
     (include-css "/vendor/font-awesome/css/font-awesome.min.css")
-    [:style css]
+    (include-css "/style.css")
     (include-js "/vendor/bootstrap/js/bootstrap.min.js")
     (include-js "/app.js")]
    [:body
@@ -35,6 +33,7 @@
 
 (defroutes app-routes
   (GET "/" [] (index-page))
+  (GET "/style.css" [] (css))
   (route/resources "/")
   (route/not-found "Not Found"))
 
