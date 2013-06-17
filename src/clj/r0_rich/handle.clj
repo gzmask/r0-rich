806319b3bda36598a582abbc0e1250e44c4455d3
gzmask <gzmask@gmail.com>
2013-06-17 16:37:01 -0600

    remove gaka, add basic menuing

--- src/clj/r0_rich/handle.clj
+++ src/clj/r0_rich/handle.clj
@@ -3,18 +3,37 @@
         hiccup.core
         hiccup.page
         ring.adapter.jetty)
-  (:require [gaka [core :as gaka]]
-            [compojure.handler :as handler]
+  (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
 (defn css [] 
   {:headers {"Content-Type" "text/css"}
-   :body (gaka/css [:body :background-color "#444"
-                   [:div.navigation_bar :background-color "#ccc" 
-                                        :color "#fff" 
-                                        :height "25px" 
-                                        :padding "5px 5px 0px 50px"]
-                   [:i.icon-camera-retro :color "#000"]])})
+   :body "body {
+            background-color: #444;
+            color: #E0771B;
+            font-family: advent-Bd1;}
+          div.content {
+            padding: 15px 15px 50px 50px;}
+          div.navigation_bar {
+            background-color: #ccc;
+            font-family: advent-Re;
+            font-size: 26px;
+            color: #fff;
+            padding: 5px 5px 2px 50px;}
+          @font-face {
+            font-family: hildieb;
+            src: url('fonts/hildieb.ttf');}
+          @font-face {
+            font-family: advent-Re;
+            src: url('fonts/advent-Re.otf');}
+          @font-face {
+            font-family: advent-Lt1;
+            src: url('fonts/advent-Lt1.otf');}
+          @font-face {
+            font-family: advent-Bd1;
+            src: url('fonts/advent-Bd1.otf');}
+          i.icon-camera-retro {
+            color: #000;} "})
 
 (defn index-page []
   "index page for richever"
@@ -28,7 +47,19 @@
     (include-js "/vendor/bootstrap/js/bootstrap.min.js")
     (include-js "/app.js")]
    [:body
-    [:div.row-fluid.navigation_bar [:i.icon-camera-retro] "&nbsp; this is navigation bar"]
+    [:div.row-fluid.navigation_bar [:div.span2 [:a [:i.icon-home] "Home"]]
+                                   [:div.span2 [:a [:i.icon-fighter-jet] "Getting Start"]]
+                                   [:div.span2 [:a [:i.icon-folder-open] "Portfolio"]]
+                                   [:div.span2 [:a [:i.icon-lightbulb] "About"]]]
+    [:div.row-fluid.content [:h1 "Simple stuff"]
+                            [:div.row-fluid.home [:h2.offset1 "Home"]
+                                                 [:div.row-fluid [:div.offset2.span2 "this is home"]]]
+                            [:div.row-fluid.start [:h2.offset1 "Getting Start"]
+                                                 [:div.row-fluid [:div.offset2.span2 "this is start"]]]
+                            [:div.row-fluid.port [:h2.offset1 "Portfolio"]
+                                                 [:div.row-fluid [:div.offset2.span2 "this is port"]]]
+                            [:div.row-fluid.about [:h2.offset1 "About"]
+                                                 [:div.row-fluid [:div.offset2.span2 "this is about"]]]]
     ]))
 
 (defroutes app-routes
