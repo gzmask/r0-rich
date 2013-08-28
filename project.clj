4f1e74199f5b4f325bea29bf88221e9f8e918619
gzmask <gzmask@gmail.com>
2013-08-28 08:43:44 -0700

    update clojar of engine

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.gzmask/richweb "0.1.0-SNAPSHOT"
+(defproject org.clojars.gzmask/richweb "0.1.0-beta1"
   :description "richever.ca"
   :url "http://richever.ca"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
   :dependencies [[ring/ring-core "1.1.8"]
                  [ring/ring-jetty-adapter "1.1.8"]
                  [enlive "1.1.1"]
-                 [org.clojars.gzmask/rich_pos "0.1.0-SNAPSHOT"]
+                 [org.clojars.gzmask/rich_pos "0.1.0-beta1"]
                  [compojure "1.1.5"]
                  [org.clojure/clojure "1.5.1"]]
   :source-paths ["src/clj"]
